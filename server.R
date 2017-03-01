36c82d2f7335e276eb820d97ee4e31552662626e
Dustin Langner <dustin.langner1@gmail.com>
2017-03-01 13:36:34 -0800

    create ui and server files

---
+++ server.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+my.server <- function(input, output) {
+  
+  
+  
+  
+}
+shinyServer(my.server)
---
+++ ui.R
@@ -0,0 +1,16 @@
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(rsconnect)
+
+my.ui <- fluidPage(
+  
+  
+  
+  
+  
+  
+  
+  
+)
+shinyUI(my.ui)
